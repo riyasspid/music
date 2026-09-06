927cab1a769979fafb005a111d444c38bc080cb5
Riyas <riyaszingbizz@gmail.com>
2026-09-06 18:25:30 +0530

    fix(android): force compileSdk 36 and enable desugaring for file_picker and plugins

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.music"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,31 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val configureAndroid = {
+        val android = project.extensions.findByName("android")
+        if (android != null) {
+            try {
+                val setCompileSdk = android.javaClass.getMethod("setCompileSdk", java.lang.Integer::class.java)
+                setCompileSdk.invoke(android, 36)
+            } catch (_: Throwable) {
+                try {
+                    val compileSdkVersion = android.javaClass.getMethod("compileSdkVersion", Int::class.javaPrimitiveType)
+                    compileSdkVersion.invoke(android, 36)
+                } catch (_: Throwable) {}
+            }
+        }
+    }
+
+    if (state.executed) {
+        configureAndroid()
+    } else {
+        afterEvaluate {
+            configureAndroid()
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
